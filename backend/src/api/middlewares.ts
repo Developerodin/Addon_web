a26b2d1dda1f7f794c43624186a2784a211203eb
akshaypareek01 <akshay96102@gmail.com>
2025-11-03 16:44:14 +0530

    working

--- backend/src/api/middlewares.ts
+++ backend/src/api/middlewares.ts
@@ -9,6 +9,30 @@ import { storeMiddlewares } from "./store/middlewares";
 
 export default defineMiddlewares({
   routes: [
+    {
+      matcher: "/*",
+      middlewares: [
+        (req: MedusaRequest, _res: MedusaResponse, next: MedusaNextFunction) => {
+          const publicUrl = process.env.MEDUSA_BACKEND_URL;
+          if (publicUrl) {
+            try {
+              const u = new URL(publicUrl);
+              if (!req.headers["x-forwarded-host"]) {
+                req.headers["x-forwarded-host"] = u.host;
+              }
+              if (!req.headers["x-forwarded-proto"]) {
+                req.headers["x-forwarded-proto"] = u.protocol.replace(":", "");
+              }
+              const prefix = u.pathname && u.pathname !== "/" ? u.pathname : "";
+              if (prefix && !req.headers["x-forwarded-prefix"]) {
+                req.headers["x-forwarded-prefix"] = prefix;
+              }
+            } catch {}
+          }
+          next();
+        },
+      ],
+    },
     ...adminMiddlewares,
     ...storeMiddlewares,
     {
